192ddbb781d160d4c0f3a7917179672c095fd772
qsQ238 <82689986+qsQ238@users.noreply.github.com>
2021-10-09 16:36:19 +0800

    Update app.js

--- call-app-master/demo/app.js
+++ call-app-master/demo/app.js
@@ -6,7 +6,7 @@ btn2.addEventListener('click', function () {
   callApp.start({
     path: '', // 兼容app所有统跳地址
     channelId: '777',
-    targetApp: 'zz',
+    targetApp: 'chinaunicom',
     wechatStyle: 1, // 1表示浮层右上角，2表示浮层按钮
     universal: true,
   })
