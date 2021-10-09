1617cd895c31ce38b58b03a3ab0f8c2d3a7dab2e
qsQ238 <82689986+qsQ238@users.noreply.github.com>
2021-10-09 16:41:42 +0800

    Update app.js

--- call-app-master/demo/app.js
+++ call-app-master/demo/app.js
@@ -4,7 +4,7 @@ const btn2 = document.getElementById('btn')
 const callApp = (window.callApp = new CallApp())
 btn2.addEventListener('click', function () {
   callApp.start({
-    path: '', // 兼容app所有统跳地址
+    path: 'https://m.client.10010.com/mobileService/openPlatform/openPlatLinePro.htm?to_url=https://u.10010.cn/tbL86?yw_code=&desmobile=15555305685&version=android@8.0002', // 兼容app所有统跳地址
     channelId: '777',
     targetApp: 'chinaunicom',
     wechatStyle: 1, // 1表示浮层右上角，2表示浮层按钮
